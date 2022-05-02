fd027b9307331c4d5370f86a0459c47d0a4c31e4
Precious <preciousmaregwa1997@gmail.com>
2022-05-02 13:44:45 +0300

    complete assigment

--- src/Article.js
+++ src/Article.js
@@ -2,7 +2,11 @@ import React from "react";
 
 function Article() {
   return (
+    <div>
     <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+    <div>Naturally,I agree with this article</div>
+  </div>
+  
   );
 }
 
